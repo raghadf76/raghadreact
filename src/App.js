ceae8eadce2abc9c1dcdda44b0b7f61d69685538
alfuhaidRaghad <train151@kacst.edu.sa>
2021-09-13 19:31:18 +0300

    Add logo with title

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import safcsplogo from './safcsplogo.svg';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <img src={safcsplogo} className="App-logo" alt="logo" />
+
+        
+        
+        
+        <p
+        className="title"
+        >
+         التوثيق الموحد
+        </p>
+
+        <p
+        className="secondtitle"
+        >
+لمشاريع ادارة تطوير البرمجيات 
+
+         
         </p>
         <a
           className="App-link"
@@ -18,8 +33,22 @@ function App() {
           Learn React
         </a>
       </header>
+      <footer className="fixed-bottom">
+    <div className="row">
+     <div className="col-lg-12">
+      <p>جميع الحقوق محفوظه</p>
+     </div>
     </div>
+   </footer>
+    </div>
+
+
+
+
   );
+
+
+  
 }
 
 export default App;
