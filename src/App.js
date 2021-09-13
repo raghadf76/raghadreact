2c7d1feca5bd780acabcb3e92e2142388e0a609d
alfuhaidRaghad <train151@kacst.edu.sa>
2021-09-14 02:49:04 +0300

    add 2 row

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 import safcsplogo from './safcsplogo.svg';
+import whiteBook from './whiteBook.png';
+import book from './book.png';
+import Footer from "./Footer";
+import {
+  Card, CardImg, CardText, CardBody,
+  CardTitle, CardSubtitle, Button,Row,Grid
+} from 'reactstrap';
 
 function App() {
   return (
     <div className="App">
+
+      {/* Header */}
       <header className="App-header">
         <img src={safcsplogo} className="App-logo" alt="logo" />
 
-        
-        
-        
         <p
         className="title"
         >
@@ -18,35 +24,135 @@ function App() {
         </p>
 
         <p
-        className="secondtitle"
-        >
-لمشاريع ادارة تطوير البرمجيات 
-
-         
+        className="secondtitle">
+          لمشاريع ادارة تطوير البرمجيات   
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
-      <footer className="fixed-bottom">
-    <div className="row">
-     <div className="col-lg-12">
-      <p>جميع الحقوق محفوظه</p>
-     </div>
-    </div>
-   </footer>
-    </div>
+      {/* Header */}
 
 
+
+    {/* Body */}
+    {/* First row */}
+  <div className="content">
+      <div className="listCard1">
+        <div className="cardTitle" >  التوثيق </div>
+      <div className="row">
+      <div className="col-md-4">
+      <Card className="card1">
+        <CardImg top width="100%" src={whiteBook}  alt="Card image cap" />
+        <CardBody className="cardBody">
+          <CardTitle tag="h5">التوثيق ١</CardTitle>
+          
+         
+        </CardBody>
+      </Card>
+      </div>
+
+      <div className="col-md-4">
+        <Card className="card1">
+        <CardImg top width="100%" src={book}  alt="Card image cap" />
+        <CardBody className="cardBody">
+          <CardTitle tag="h5">التوثيق ٢</CardTitle>
+          
+         
+        </CardBody>
+      </Card>
+      </div>
+
+      <div className="col-md-4">
+        <Card className="card1">
+        <CardImg top width="100%" src={book}  alt="Card image cap" />
+        <CardBody className="cardBody">
+          <CardTitle tag="h5">التوثيق ٣</CardTitle>
+          
+         
+        </CardBody>
+      </Card>
+      </div>
+</div>
+
+</div>
+
+
+
+{/* Second row */}
+
+<div className="listCard2">
+        <div className="cardTitle" >  مشاريعنا </div>
+      <div className="row">
+      <div className="col-md-4">
+        <Card className="card1">
+        <CardImg top width="100%" src={whiteBook}  alt="Card image cap" />
+        <CardBody className="cardBody">
+          <CardTitle tag="h5">مشروع ١ </CardTitle>
+          
+         
+        </CardBody>
+      </Card>
+      </div>
+
+      <div className="col-md-4">
+<Card className="card1">
+        <CardImg top width="100%" src={book}  alt="Card image cap" />
+        <CardBody className="cardBody">
+          <CardTitle tag="h5">مشروع ٢</CardTitle>
+          
+         
+        </CardBody>
+      </Card>
+      </div>
+
+      <div className="col-md-4">
+<Card className="card1">
+        <CardImg top width="100%" src={book}  alt="Card image cap" />
+        <CardBody className="cardBody">
+          <CardTitle tag="h5">مشروع ٣</CardTitle>
+          
+         
+        </CardBody>
+      </Card>
+      </div>
+
+
+      <div className="col-md-4">
+<Card className="transparentCard">
+        <CardImg top width="100%" src={whiteBook}  alt="Card image cap" />
+        <CardBody className="cardBody">
+          <CardTitle tag="h5">مشروع ٤</CardTitle>
+          <div class='text-on-image'>
+قريبا
+          </div>
+         
+        </CardBody>
+      </Card>
+      </div>
+
+
+      <div className="col-md-4">
+<Card className="transparentCard">
+        <CardImg top width="100%" src={whiteBook}  alt="Card image cap" />
+        <div class='text-on-image'>
+قريبا
+          </div>
+        <CardBody className="cardBody">
+          <CardTitle tag="h5">مشروع ٥</CardTitle>
+          
+         
+        </CardBody>
+      </Card>
+      </div>
+</div>
+
+</div>
+
+</div>
+<Footer />
+    </div>
 
 
   );
-
 
   
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,6 +12,8 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+
+document.getElementsByTagName('html')[0].setAttribute("dir", "rtl");
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
